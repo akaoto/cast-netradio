1cae1ffd066e7e0022a507a48b9a706ec5d38930
akaoto <redmusic@outlook.com>
2017-12-25 00:10:33 +0900

    Fix bug.

--- cast_netradio/param.py
+++ cast_netradio/param.py
@@ -1,6 +1,6 @@
 server_host = '192.168.0.4'
 chromecast_host = '192.168.0.7'
-bbt_hostname = 'api.beebotte.com',
+bbt_hostname = 'api.beebotte.com'
 bbt_channel = 'ifttt_radio'
 bbt_resource = 'radio_title'
 bbt_token = 'input_bbt_token'
